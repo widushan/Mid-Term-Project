9d7206f467dc5ac21a9e58054f83a1fb5bd4f40f
widushan <114930601+widushan@users.noreply.github.com>
2025-01-24 19:28:39 +0530

    add comment

--- lib/screens/book_list.dart
+++ lib/screens/book_list.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mid_term_project/widgets/book.dart';
 
-
-
+// classes
 class BookList extends StatelessWidget {
   const BookList({super.key});
 
@@ -15,60 +14,58 @@ class BookList extends StatelessWidget {
           backgroundColor: Colors.blueGrey,
         ),
         body: SafeArea(
-          child: ListView(
-  children: [
-    // First Book Item
-    Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Book(
-          coverImagePath: 'assets/images/rich-dad-poor-dad.jpg',
-          title: 'Rich Dad, Poor Dad',
-          author: 'Robert T. Kiyosaki',
-          price: 1250,
-        ),
-        const Divider(
-          thickness: 1, // Adjust thickness as needed
-          color: Colors.grey, // Adjust color as needed
-        ),
-      ],
-    ),
-    // Second Book Item
-    Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Book(
-          coverImagePath: 'assets/images/the-alchemist.jpg',
-          title: 'The Alchemist',
-          author: 'Coelho Paulo',
-          price: 1000,
-        ),
-        const Divider(
-          thickness: 1,
-          color: Colors.grey,
-        ),
-      ],
-    ),
-    // Third Book Item
-    Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Book(
-          coverImagePath: 'assets/images/men-are-from-mars.jpg',
-          title: 'Men are From Mars',
-          author: 'John Gray',
-          price: 1500,
-        ),
-        const Divider(
-          thickness: 1,
-          color: Colors.grey,
-        ),
-      ],
-    ),
-  ],
-)
-
-        ),
+            child: ListView(
+          children: [
+            // First Book Item
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Book(
+                  coverImagePath: 'assets/images/rich-dad-poor-dad.jpg',
+                  title: 'Rich Dad, Poor Dad',
+                  author: 'Robert T. Kiyosaki',
+                  price: 1250,
+                ),
+                const Divider(
+                  thickness: 1, // Adjust thickness as needed
+                  color: Colors.grey, // Adjust color as needed
+                ),
+              ],
+            ),
+            // Second Book Item
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Book(
+                  coverImagePath: 'assets/images/the-alchemist.jpg',
+                  title: 'The Alchemist',
+                  author: 'Coelho Paulo',
+                  price: 1000,
+                ),
+                const Divider(
+                  thickness: 1,
+                  color: Colors.grey,
+                ),
+              ],
+            ),
+            // Third Book Item
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Book(
+                  coverImagePath: 'assets/images/men-are-from-mars.jpg',
+                  title: 'Men are From Mars',
+                  author: 'John Gray',
+                  price: 1500,
+                ),
+                const Divider(
+                  thickness: 1,
+                  color: Colors.grey,
+                ),
+              ],
+            ),
+          ],
+        )),
       ),
     );
   }
